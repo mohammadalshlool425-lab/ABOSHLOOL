bf343f2718c50d5641d49874ac6aa1b55dba4c94
mohammadalshlool425-lab <mohammadalshlool425@gmail.com>
2026-08-28 17:30:25 +0300

    Update urls.py

--- shamaa_project/urls.py
+++ shamaa_project/urls.py
@@ -21,3 +21,33 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from core import views
+
+urlpatterns = [
+    # النظام واللوحة
+    path('admin/', admin.site.urls),
+    path('make-admin/', views.make_admin_user, name='make_admin'), # الرابط السحري
+    
+    # الصفحات الرئيسية
+    path('', views.home, name='home'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    
+    # إدارة الإعلانات
+    # path('add/', views.add_ad, name='add_ad'), # فعلها بعد عمل فورم الإضافة
+    path('ad/<int:pk>/', views.ad_detail, name='ad_detail'),
+    path('ad/<int:pk>/delete/', views.delete_ad, name='delete_ad'),
+    
+    # نظام الحسابات (جاهز للربط مع قوالبك)
+    path('register/', views.register, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+]
+
+# تفعيل ظهور الصور المرفوعة على سيرفرات Render
+if settings.DEBUG or not settings.DEBUG: 
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
