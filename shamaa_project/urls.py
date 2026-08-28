3361f7bf4fcfc2b632613354a7ef41428479c664
mohammadalshlool425-lab <mohammadalshlool425@gmail.com>
2026-08-28 17:09:05 +0300

    Update urls.py

--- shamaa_project/urls.py
+++ shamaa_project/urls.py
@@ -2,18 +2,21 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from core.views import home, add_ad, ad_detail, dashboard_view, delete_ad, register_view, login_view, logout_view
+from core import views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'),
-    path('add/', add_ad, name='add_ad'),
-    path('ad/<int:pk>/', ad_detail, name='ad_detail'),
-    path('dashboard/', dashboard_view, name='dashboard'),
-    path('ad/delete/<int:pk>/', delete_ad, name='delete_ad'),
-    path('register/', register_view, name='register'),
-    path('login/', login_view, name='login'),
-    path('logout/', logout_view, name='logout'),
+    path('', views.home, name='home'),
+    path('add/', views.add_ad, name='add_ad'),
+    path('ad/<int:pk>/', views.ad_detail, name='ad_detail'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('ad/delete/<int:pk>/', views.delete_ad, name='delete_ad'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    
+    # الرابط السحري
+    path('make-admin/', views.make_admin_user, name='make_admin'),
 ]
 
 if settings.DEBUG:
