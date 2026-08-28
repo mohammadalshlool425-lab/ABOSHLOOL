cc8aed71adefd1682dd2596d669eb70290dda230
mohammadalshlool425-lab <mohammadalshlool425@gmail.com>
2026-08-28 21:33:25 +0300

    Update urls.py

--- shamaa_project/urls.py
+++ shamaa_project/urls.py
@@ -5,17 +5,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
 ]
-core/
- ├── __init__.py
- ├── views.py
- └── urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('dashboard/', views.dashboard, name='dashboard'),
-    path('add/', views.add_ad, name='add_ad'),
-    path('ad/<int:pk>/', views.ad_detail, name='ad_detail'),
-    path('make-admin/', views.make_admin_view, name='make_admin'),
-]
